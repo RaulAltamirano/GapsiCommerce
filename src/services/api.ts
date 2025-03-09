99a0a68b16229ff99dd063e225cbf01963cd00e2
Pacman <rulomania2011@hotmail.com>
2025-03-08 21:06:49 -0600

    fixed(api) add .env to secret

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,10 +1,11 @@
 // src/services/api.js
 import axios from 'axios';
 
+const apiKey = process.env.RAPIDAPI_KEY;
 const api = axios.create({
   baseURL: 'https://axesso-walmart-data-service.p.rapidapi.com',
   headers: {
-    'x-rapidapi-key': 'fce0e15738msh6a87c0c9db9505cp14b74fjsn54bc768f3bc7',
+    'x-rapidapi-key': apiKey, 
     'x-rapidapi-host': 'axesso-walmart-data-service.p.rapidapi.com',
     'Content-Type': 'application/json',
   },
